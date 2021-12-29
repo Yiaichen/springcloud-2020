ae10c2ad77f30e2f50f96c76b0a68f3bcee29d5c
vayi <chu@ittx.com.cn>
2021-12-29 17:27:23 +0800

    增加consumer-feign-hystrix进行测试

---
+++ cloud-consumer-feign-hystrix-order80/src/main/java/com/vayi/springcloud/OrderHystrixMain80.java
@@ -0,0 +1,19 @@
+package com.vayi.springcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
+
+/**
+ * @Description: TODO
+ * @author: vayi
+ * @date: 2021年11月19日 16:11
+ */
+@SpringBootApplication
+@EnableFeignClients
+public class OrderHystrixMain80 {
+    public static void main(String[] args) {
+        SpringApplication.run(OrderHystrixMain80.class,args);
+    }
+}
+
