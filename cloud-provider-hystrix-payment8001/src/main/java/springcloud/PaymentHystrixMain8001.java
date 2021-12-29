730d0a8c3b0361473e30e645e92dcd219bbcd3cc
vayi <chu@ittx.com.cn>
2021-12-29 17:18:51 +0800

    测试微服务超市场景

---
+++ cloud-provider-hystrix-payment8001/src/main/java/springcloud/PaymentHystrixMain8001.java
@@ -0,0 +1,21 @@
+package springcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+
+/**
+ * PaymentMain8001主启动类
+ *
+ * @author vayi
+ * @version 1.0
+ * @date 2020/12/02
+ */
+@SpringBootApplication
+@EnableEurekaClient
+public class PaymentHystrixMain8001 {
+    public static void main(String[] args) {
+        SpringApplication.run(PaymentHystrixMain8001.class,args);
+    }
+}
+
