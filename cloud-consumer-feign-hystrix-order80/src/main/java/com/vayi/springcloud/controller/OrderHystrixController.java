61913a96e1651589aa0910a2b5ba1b5404c3d3b9
vayi <584947742@qq.com>
2021-12-29 20:43:41 +0800

    服务生产者跟消费者 增加hystrix降级

--- cloud-consumer-feign-hystrix-order80/src/main/java/com/vayi/springcloud/controller/OrderHystrixController.java
+++ cloud-consumer-feign-hystrix-order80/src/main/java/com/vayi/springcloud/controller/OrderHystrixController.java
@@ -1,5 +1,7 @@
 package com.vayi.springcloud.controller;
 
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixProperty;
 import com.vayi.springcloud.service.PaymentHystrixService;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
@@ -25,11 +27,20 @@ public class OrderHystrixController {
         log.info("*******result:"+result);
         return result;
     }
+
     @GetMapping("/consumer/payment/hystrix/timeout/{id}")
+    @HystrixCommand(fallbackMethod = "paymentTimeOutFallbackMethod",commandProperties = {
+            @HystrixProperty(name = "execution.isolation.thread.timeoutInMilliseconds",value = "1500")  //3秒钟以内就是正常的业务逻辑
+    })
     public String paymentInfo_TimeOut(@PathVariable("id") Integer id){
         String result = paymentHystrixService.paymentInfo_TimeOut(id);
         log.info("*******result:"+result);
         return result;
+    }
+
+    //兜底方法
+    public String paymentTimeOutFallbackMethod(@PathVariable("id") Integer id){
+        return "我是消费者80，对付支付系统繁忙请10秒钟后再试或者自己运行出错请检查自己,(┬＿┬)";
     }
 
 }
